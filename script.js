c911e1f0cd114a7f5d5a96bc80664ea5e8afc7c3
C-Dresser <142123153+C-Dresser@users.noreply.github.com>
2023-11-27 18:25:26 -0500

    fixed script link location in html and addedan event listener to the search button

--- script.js
+++ script.js
@@ -1,1 +1,11 @@
 console.log("hello");
+
+var searchHistoryContainer = document.getElementById("searchHistoryContainer");
+var searchHistory = [""];
+var searchButton = document.getElementById("searchButton");
+var searchInput = document.getElementById("searchInput");
+
+
+searchButton.addEventListener('click', function (event) {
+        console.log(searchInput.value);
+});
