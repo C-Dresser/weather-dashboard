20e386b409b5e26a31513016ff19a4311597981b
C-Dresser <142123153+C-Dresser@users.noreply.github.com>
2023-11-27 18:47:19 -0500

    updated event listener to add user input to an array

--- script.js
+++ script.js
@@ -1,11 +1,18 @@
 console.log("hello");
 
+//Housekeeping
 var searchHistoryContainer = document.getElementById("searchHistoryContainer");
 var searchHistory = [""];
 var searchButton = document.getElementById("searchButton");
 var searchInput = document.getElementById("searchInput");
 
-
+//added event listener to search button
+//added functionality to event listner to log user input to an array
 searchButton.addEventListener('click', function (event) {
-        console.log(searchInput.value);
+    var inputValue = searchInput.value.trim();
+    if (inputValue !== "") {
+        searchHistory.push(inputValue);
+        console.log("Search history:", searchHistory);
+            
+        }
 });
