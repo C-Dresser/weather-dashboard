9baafce2d45b627783f08feee7348940facd4875
C-Dresser <142123153+C-Dresser@users.noreply.github.com>
2023-11-27 19:07:55 -0500

    updated script to append user input as buttons in search history and cap the amount of buttons so things dont get out of control. also fixed a typo in the html

--- script.js
+++ script.js
@@ -5,14 +5,31 @@ var searchHistoryContainer = document.getElementById("searchHistoryContainer");
 var searchHistory = [""];
 var searchButton = document.getElementById("searchButton");
 var searchInput = document.getElementById("searchInput");
+var maxHistorySize = 6;
 
+//added function to append the array values to the page as buttons
+function updateSearchHistory() {
+    searchHistoryContainer.innerHTML = "";
+    searchHistory.slice(-maxHistorySize).forEach(function (entry) {
+        var entryButton = document.createElement("button");
+        entryButton.textContent = entry;
+        searchHistoryContainer.appendChild(entryButton);
+    });
+}
 //added event listener to search button
 //added functionality to event listner to log user input to an array
+//updated the function to be used inline with the updateSearchHistory function. With the new maxHistory var, search history is capped at 6 so things dont get out of control. I may change this number later depending on how it looks with all of the styling completed. 
 searchButton.addEventListener('click', function (event) {
     var inputValue = searchInput.value.trim();
+
     if (inputValue !== "") {
         searchHistory.push(inputValue);
-        console.log("Search history:", searchHistory);
-            
+
+        if (searchHistory.length > maxHistorySize) {
+            searchHistory.shift();
         }
+
+        updateSearchHistory();
+        console.log("Search history:", searchHistory);
+    }
 });
